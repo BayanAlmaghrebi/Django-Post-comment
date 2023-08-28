cea75d9c2f819ce547c6cf78736d684398901221
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-08-28 21:04:27 +0200

    add taggit

--- Questions/models.py
+++ Questions/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from taggit.managers import TaggableManager
+
 
 
 # Create your models here.
@@ -20,14 +22,16 @@ Answer :
 '''
 
 class Question(models.Model):
-    user = models.ForeignKey(User,related_name='post_user' , on_delete=models.SET_NULL,null=True)
+    user = models.ForeignKey(User,related_name='Question_user' , on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=120)
     content = models.TextField(max_length=30000)
     created_at = models.DateTimeField()
+    tags = TaggableManager()
 
 
 class Answer(models.Model):
-    user = models.ForeignKey(User,related_name='post_user' , on_delete=models.SET_NULL,null=True)
+    user = models.ForeignKey(User,related_name='Answer_user' , on_delete=models.SET_NULL,null=True)
     question = models.CharField(max_length=500)
     answer = models.TextField(max_length=30000)
     created_at = models.DateTimeField()
+    tags = TaggableManager()
